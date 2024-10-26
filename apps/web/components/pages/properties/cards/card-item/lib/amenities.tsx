c313520a58ae85e28967fa8c6a61a828a43eb459
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-26 20:35:41 +0300

    refactor(web): card

--- apps/web/components/pages/properties/cards/card-item/lib/amenities.tsx
+++ apps/web/components/pages/properties/cards/card-item/lib/amenities.tsx
@@ -3,7 +3,7 @@ import DisplayAmenityIcon from "@/components/commons/display-amenity-icon";
 
 const Amenities = ({ property }: { property: TProperty }) => {
   return (
-    <div className="flex items-center justify-start gap-4 mb-2 ml-1 max-w-[85vw] overflow-x-auto overflow-hidden">
+    <div className="flex items-center justify-start gap-4 mb-2 ml-1 max-w-[85vw] overflow-x-auto overflow-hidden pb-1">
       {property?.amenity?.facilities?.map((id: number, index: number) => (
         <DisplayAmenityIcon key={index} amenity={id} />
       ))}
