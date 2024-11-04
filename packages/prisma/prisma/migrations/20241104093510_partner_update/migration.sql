1719aa5974f366d2da5f115b43c50fe45b2ade4e
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-04 12:46:26 +0300

    refactor(web): update partner schema

---
+++ packages/prisma/prisma/migrations/20241104093510_partner_update/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Partner" ADD COLUMN     "commision" INTEGER NOT NULL DEFAULT 5,
+ADD COLUMN     "name" TEXT NOT NULL DEFAULT 'Hotel';
---
+++ packages/prisma/prisma/migrations/20241104093607_partner/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Partner" ALTER COLUMN "name" DROP DEFAULT;
