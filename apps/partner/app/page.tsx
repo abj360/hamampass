a3abaf6a2854e145608bdb56a15e1b07f25f41ce
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-05 08:47:56 +0300

    refactor(web): matcher test

--- apps/partner/app/page.tsx
+++ apps/partner/app/page.tsx
@@ -3,17 +3,23 @@ import moment from "moment";
 import "moment/locale/tr";
 
 const Home = async () => {
+  const partnerParam = "0001";
+
   moment.locale("tr");
 
   const partner = await prisma.partner.findUnique({
     where: {
-      partnerId: "0001",
+      partnerId: partnerParam,
     },
     include: {
-      bookings: true,
+      bookings: {
+        include: {
+          user: true,
+          property: true,
+        },
+      },
     },
   });
-  console.log(partner);
 
   return (
     <main className="flex flex-col items-center mt-2 mx-5">
@@ -21,12 +27,40 @@ const Home = async () => {
       {partner?.bookings.map((booking) => (
         <div
           key={booking.id}
-          className="border p-2 m-2 rounded bg-gray-200 w-full flex items-center justify-between"
+          className="border p-2 m-2 rounded bg-gray-200 w-full flex flex-col items-start"
         >
-          <p className="text-sm">{moment(booking.date).format("D MMMM")}</p>
-          <p className="bg-red-700 text-white px-2 py-1 rounded text-sm">
-            ödenmedi
-          </p>
+          <div className="flex items-center justify-between w-full">
+            {" "}
+            {/* Booking Date */}
+            <p className="text-sm mb-1">
+              {moment(booking.date).format("D MMMM")}
+            </p>
+            {/* Booking Status */}
+            <p className="bg-red-700 text-white px-2 py-1 rounded text-sm mb-1">
+              Ödenmedi
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between w-full">
+            {/* User Information */}
+            <div>{booking.property.title}</div>
+            <div className=" mb-1">{booking.user?.name}</div>
+          </div>
+
+          {/* Total Money */}
+          <div className="text-sm mt-2">
+            <strong>Toplam Ödeme:</strong> {booking.totalMoney}₺
+          </div>
+          <div className="mt-2 w-full  flex items-center">
+            <strong className="mr-auto">Komisyon miktarı:</strong>{" "}
+            <p>
+              {" "}
+              {booking.totalMoney} x %{partner.commision} ={" "}
+              <span className="text-lg font-bold">
+                {(booking.totalMoney * partner.commision) / 100}₺
+              </span>
+            </p>
+          </div>
         </div>
       ))}
     </main>
