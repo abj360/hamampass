d8b280f8a961090a89712a9d48862b7e589eb5cc
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-05 14:35:01 +0300

    refactor(web): matcher test

--- apps/partner/app/api/[partnerId]/route.ts
+++ apps/partner/app/api/[partnerId]/route.ts
@@ -15,7 +15,7 @@ export async function GET(req: NextRequest) {
     }
 
     console.log("Fetching partner data from database...");
-    const fetchedPartner = await prisma.partner.findFirst({
+    const fetchedPartner = await prisma.partner.findUnique({
       where: {
         id: partnerId,
       },
