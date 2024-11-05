8babdc603f0d71770cea07ae9b9ec2154f5f1f47
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-05 14:38:25 +0300

    refactor(web): matcher test

--- apps/partner/next.config.ts
+++ apps/partner/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import prismaVercelConfig from "@hamampass/db/prisma/index.mjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  ...prismaVercelConfig,
 };
 
 export default nextConfig;
