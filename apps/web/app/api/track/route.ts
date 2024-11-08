8f76750936cf48fb3cd1745dbfddad71f0124337
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-08 18:16:57 +0300

    feat(web): mixpanel hook setup for unique user

---
+++ apps/web/app/api/track/route.ts
@@ -0,0 +1,45 @@
+// app/api/track/route.js
+import { NextResponse } from "next/server";
+import Mixpanel from "mixpanel";
+import { v4 as uuidv4 } from "uuid";
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+// Initialize Mixpanel with your project token
+const mixpanel = Mixpanel.init("85d729681726f246c0c43d6611a95d1e");
+
+export async function POST(req: NextRequest) {
+  // Extract request data
+  const { event, properties } = await req.json();
+
+  // Set up cookies to manage user ID
+  const cookieStore = cookies();
+  let userId = cookieStore.get("user_id")?.value;
+
+  // If user_id cookie is missing, generate and set a new one
+  if (!userId) {
+    userId = uuidv4();
+    cookieStore.set("user_id", userId, {
+      httpOnly: true,
+      maxAge: 365 * 24 * 60 * 60 * 1000, // 1-year expiration
+    });
+  }
+
+  // Track the event in Mixpanel with the user's unique ID
+  try {
+    mixpanel.track(event, {
+      distinct_id: userId,
+      ...properties,
+    });
+    return NextResponse.json(
+      { message: "Event tracked successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error tracking event:", error);
+    return NextResponse.json(
+      { message: "Error tracking event" },
+      { status: 500 }
+    );
+  }
+}
--- apps/web/components/pages/home/index.tsx
+++ apps/web/components/pages/home/index.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import BannerSection from "./banner";
 import HeroSection from "./hero";
 import CardSection from "./card";
 import StickyHeader from "./sticky-header";
 import { useSearchParams } from "next/navigation";
+import useTrack from "@/hooks/useTrack";
 
 const HomePage = ({ locale }: { locale: string }) => {
   const [isStickyVisible, setIsStickyVisible] = useState(false);
   const searchParams = useSearchParams();
   const partnerParam = searchParams?.get("partner");
+  const track = useTrack();
+
+  track({ event: "home page loaded" });
 
   useEffect(() => {
     const handleScroll = () => {
