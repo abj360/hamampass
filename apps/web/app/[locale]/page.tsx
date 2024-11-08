49d0b261fe8d07faec8fa9160499a422e82a86ca
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-08 11:35:16 +0300

    feat(web): mixpanel setup

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,6 +1,10 @@
 import HomePage from "@/components/pages/home";
+import mixpanel from "@/utils/mixpanel";
 
 export default function Home({ params }: any) {
   const { locale } = params;
+
+  mixpanel.track("Home Page Viewed");
+
   return <HomePage locale={locale} />;
 }
---
+++ apps/web/utils/mixpanel.ts
@@ -0,0 +1,5 @@
+import Mixpanel from "mixpanel";
+
+const mixpanel = Mixpanel.init("85d729681726f246c0c43d6611a95d1e");
+
+export default mixpanel;
