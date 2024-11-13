68b7f95fe1a65b2ab8f05ae2921d58e8394b7266
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-13 12:42:08 +0300

    test(web): sticky footer implementation

--- apps/web/app/[locale]/my-bookings/page.tsx
+++ apps/web/app/[locale]/my-bookings/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/commons/new-header";
 const MyBookings = () => {
   return (
     <main className="flex flex-col min-h-screen">
-      <Header variant="white" title="Reservations" />
+      <Header variant="white" isHambuger={true} />
       <MyBookingsPage />
     </main>
   );
