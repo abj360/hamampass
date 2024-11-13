d729db5ed2322aabf3492ebc88eaba4fe343e8db
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-13 15:20:30 +0300

    test(web): sticky footer implementation

--- apps/web/components/pages/single-property/image-slider/drawer.tsx
+++ apps/web/components/pages/single-property/image-slider/drawer.tsx
@@ -25,28 +25,6 @@ interface GaleryDrawerProps {
 
 const GaleryDrawer: React.FC<GaleryDrawerProps> = ({ trigger, data }) => {
   const s = useTranslations("titles");
-  const [scale, setScale] = useState(1); // Scale for zoom
-
-  // Handle mouse wheel zoom
-  const handleWheel = (e: any) => {
-    e.preventDefault();
-    let newScale = scale + e.deltaY * -0.01;
-    newScale = Math.min(Math.max(1, newScale), 3); // Limit scale between 1x and 3x
-    setScale(newScale);
-  };
-
-  // Handle pinch zoom
-  const handleTouchMove = (e: any) => {
-    if (e.touches.length === 2) {
-      const touch1 = e.touches[0];
-      const touch2 = e.touches[1];
-      const distance = Math.sqrt(
-        Math.pow(touch2.pageX - touch1.pageX, 2) +
-          Math.pow(touch2.pageY - touch1.pageY, 2)
-      );
-      setScale(distance / 100); // Adjust 100 to fit your needs
-    }
-  };
 
   return (
     <Drawer>
@@ -71,27 +49,14 @@ const GaleryDrawer: React.FC<GaleryDrawerProps> = ({ trigger, data }) => {
                     className="object-cover w-full h-full touch-auto overflow-auto"
                   />
                 </DialogTrigger>
-                <DialogContent
-                  className="p-0 border-none overflow-hidden"
-                  onWheel={handleWheel}
-                  onTouchMove={handleTouchMove}
-                >
-                  <div
-                    style={{
-                      transform: `scale(${scale})`,
-                      transformOrigin: "center center",
-                      transition: "transform 0.2s ease",
-                    }}
-                    className="w-full h-full relative"
-                  >
-                    <Image
-                      src={photo}
-                      width={1600}
-                      height={900}
-                      alt="photo"
-                      className="object-cover w-full h-full touch-auto"
-                    />
-                  </div>
+                <DialogContent className="p-0 border-none overflow-hidden">
+                  <Image
+                    src={photo}
+                    width={1600}
+                    height={900}
+                    alt="photo"
+                    className="object-cover w-full h-full touch-auto"
+                  />
                 </DialogContent>
               </Dialog>
             </div>
