534143194879e0a9eee5f5c22e521ffe4ba6e30f
ikbal arslan <ikbalarslandev@gmail.com>
2024-11-13 12:15:20 +0300

    test(web): sticky footer implementation

--- apps/web/components/commons/new-header/index.tsx
+++ apps/web/components/commons/new-header/index.tsx
@@ -9,20 +9,38 @@ import { IoChevronBack } from "react-icons/io5";
 
 interface HeaderProps {
   variant?: "default" | "sticky" | "white";
-  title?: string;
+  isHambuger?: boolean;
 }
 
-const Header = ({ variant = "default", title }: HeaderProps) => {
+const Header = ({ variant = "default", isHambuger = false }: HeaderProps) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
+  const [cartItemCount, setCartItemCount] = useState(0);
 
   const handleLogo = () => {
     router.push(`/`);
   };
 
-  const handleBack = () => {
-    router.back();
-  };
+  useEffect(() => {
+    const handleCartUpdated = () => {
+      setCartItemCount(
+        Object.values(
+          JSON.parse(localStorage.getItem("cart") || "{}")?.products || {}
+        ).reduce((acc: number, item: any) => acc + item.count, 0)
+      );
+    };
+
+    // Initial load
+    handleCartUpdated();
+
+    // Add event listener
+    window.addEventListener("cartUpdated", handleCartUpdated);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("cartUpdated", handleCartUpdated);
+    };
+  }, []);
 
   // Determine styles and logo based on the variant
   const isSticky = variant === "sticky";
@@ -51,13 +69,38 @@ const Header = ({ variant = "default", title }: HeaderProps) => {
   const backgroundColor = isWhite ? "bg-white" : "";
 
   return (
-    <header className={`${textColor} ${backgroundColor} flex `}>
+    <header
+      className={`flex justify-between items-center ${textColor} ${backgroundColor} ${isHambuger && "mx-2"}`}
+    >
       <button
         onClick={handleLogo}
-        className="flex-1 flex items-center justify-center"
+        className={`w-full ${isHambuger ? " " : "flex items-center justify-center"}`}
       >
         <Image src={logoSrc} width={200} height={200} alt="logo" />
       </button>
+
+      {isHambuger && (
+        <HamburgerDrawerComponent
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          cartItemCount={cartItemCount}
+          trigger={
+            <div className="relative">
+              <Hamburger
+                toggled={isOpen}
+                size={24}
+                aria-expanded={isOpen}
+                aria-controls="drawer-content"
+              />
+              {cartItemCount > 0 && (
+                <span className="absolute top-2 right-1 text-xs bg-secondary-700 text-center border text-white aspect-square rounded-full px-1">
+                  {cartItemCount}
+                </span>
+              )}
+            </div>
+          }
+        />
+      )}
     </header>
   );
 };
